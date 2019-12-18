44c437ff08101b11237f6e34e62c31a993685652
Hitesh Ahuja <geek.hitesh@gmail.com>
2019-12-18 17:55:41 +0530

    Update initialSetup.sh

--- initialSetup.sh
+++ initialSetup.sh
@@ -71,3 +71,10 @@ mkdir -p /opt/course/19
 ns moon
 k run nginx8 --image=nginx --restart=Never -l type=worker
 k create secret generic Secret2 --from-literal=db=mysql --dry-run -o yaml > /opt/course/14/secret2.yaml
+
+ns mars
+curl https://raw.githubusercontent.com/geekhitesh/CKAD/master/question_17.yaml >  /opt/course/17/test-init-container.yaml
+
+k apply -f  /opt/course/17/test-init-container.yaml
+
+
